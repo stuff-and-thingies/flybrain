6bb1e66b098cdca567adae3cf8aef88e46a1073d
Ben Hall <rcmast3r1@gmail.com>
2026-08-04 21:26:08 -0400

    Bhall4/flybrain nixos (#6)
    * nixos building
    
    * built
    
    * adding in more fixes for building on x86
    
    * adding in the rest of the fixes for building on x86
    
    * building
    
    * erm
    
    * reformatting

--- flake.nix
+++ flake.nix
@@ -1,15 +1,39 @@
 {
   nixConfig = {
-    extra-substituters = [ "https://ros.cachix.org" ];
-    extra-trusted-public-keys = [ "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo=" ];
+    extra-substituters = [
+      "https://ros.cachix.org"
+      "https://nixos-raspberrypi.cachix.org"
+      "https://rcmast3r.cachix.org"
+      "http://neb-cache.yeet/neb-cache"
+    ];
+    extra-trusted-public-keys = [
+      "ros.cachix.org-1:dSyZxI8geDCJrwgvCOHDoAfOm5sV1wCPjBkKL+38Rvo="
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+      "rcmast3r.cachix.org-1:dH22dF877RZ1j7uvAgqnQWNChxdQDeqgBRWpXzoi84c="
+      "neb-cache:KTaVU/xOSztooic5m8ZrvvP/5lp3Lg3Y/P7NUyZtKII="
+    ];
   };
 
   inputs = {
+
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/develop";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+    };
+
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/develop";
-    nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # IMPORTANT!!!
+    nix-ros-overlay.inputs.nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+
+    nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+
     nixgl.url = "github:nix-community/nixGL";
+    nixgl.inputs.nixpkgs.follows = "nixos-raspberrypi/nixpkgs";
+
     nix2container-src.url = "github:nlewo/nix2container";
     nix2container-src.flake = false;
+
   };
   outputs =
     {
@@ -18,56 +42,63 @@
       nixpkgs,
       nixgl,
       nix2container-src,
+      nixos-raspberrypi,
+      disko,
       ...
-    }:
+    }@inputs:
+    let
+      pkgs-overlays = [
+        nix-ros-overlay.overlays.default
+
+        (import ./nix/fix-overlay.nix { inherit nixgl; })
+
+        (final: prev: {
+          px4-gazebo-models = prev.callPackage ./nix/px4-gazebo.nix { };
+          nix2container = (prev.callPackage nix2container-src { pkgs = prev; }).nix2container;
+
+          micro-cdr = prev.callPackage ./nix/micro-cdr.nix { };
+          micro-xrce-dds-client = prev.callPackage ./nix/micro-xrce-dds-client.nix { };
+
+          fast-cdr = prev.callPackage ./nix/fast-cdr.nix { };
+          fast-dds-2 = prev.callPackage ./nix/fast-dds-2.nix { inherit (final) fast-cdr; };
+          spdlog-1_9 = prev.callPackage ./nix/spdlog-1_9.nix { };
+          micro-xrce-dds-agent = prev.callPackage ./nix/micro-xrce-dds-agent.nix {
+            inherit (final) fast-cdr fast-dds-2 spdlog-1_9;
+          };
+
+          rosPackages = prev.rosPackages // {
+            kilted = prev.rosPackages.kilted.overrideScope (
+              rFinal: _rPrev: {
+                px4-msgs = rFinal.callPackage ./nix/px4-msgs.nix { };
+                px4-ros2-cpp = rFinal.callPackage ./nix/px4-ros2-cpp.nix { };
+              }
+            );
+          };
+
+          px4-sitl = final.nix2container.buildImage {
+            name = "px4-sitl";
+            fromImage = final.nix2container.pullImage {
+              imageName = "px4io/px4-sitl";
+              imageDigest = "sha256:b6bfb9e2aece2761ff78831c9bc6f13beb2840c36ba7e010f42b58f97924d2ab";
+              sha256 = "sha256-MjDfYuxpjpFfdi5wFoItEJJjZ4PTLKvlGlNrI4iUfXc=";
+            };
+            config =
+              let
+                app = prev.writeScript "entry" (builtins.readFile ./sih-entrypoint.sh);
+              in
+              {
+                entrypoint = [ app ];
+              };
+          };
+        })
+      ];
+    in
     nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            nix-ros-overlay.overlays.default
-            nixgl.overlay
-            (final: prev: {
-              px4-gazebo-models = prev.callPackage ./nix/px4-gazebo.nix { };
-              nix2container = (prev.callPackage nix2container-src { pkgs = prev; }).nix2container;
-
-              micro-cdr = prev.callPackage ./nix/micro-cdr.nix { };
-              micro-xrce-dds-client = prev.callPackage ./nix/micro-xrce-dds-client.nix { };
-
-              fast-cdr = prev.callPackage ./nix/fast-cdr.nix { };
-              fast-dds-2 = prev.callPackage ./nix/fast-dds-2.nix { inherit (final) fast-cdr; };
-              spdlog-1_9 = prev.callPackage ./nix/spdlog-1_9.nix { };
-              micro-xrce-dds-agent = prev.callPackage ./nix/micro-xrce-dds-agent.nix {
-                inherit (final) fast-cdr fast-dds-2 spdlog-1_9;
-              };
-
-              rosPackages = prev.rosPackages // {
-                kilted = prev.rosPackages.kilted.overrideScope (
-                  rFinal: _rPrev: {
-                    px4-msgs = rFinal.callPackage ./nix/px4-msgs.nix { };
-                    px4-ros2-cpp = rFinal.callPackage ./nix/px4-ros2-cpp.nix { };
-                  }
-                );
-              };
-
-              px4-sitl = final.nix2container.buildImage {
-                name = "px4-sitl";
-                fromImage = final.nix2container.pullImage {
-                  imageName = "px4io/px4-sitl";
-                  imageDigest = "sha256:b6bfb9e2aece2761ff78831c9bc6f13beb2840c36ba7e010f42b58f97924d2ab";
-                  sha256 = "sha256-MjDfYuxpjpFfdi5wFoItEJJjZ4PTLKvlGlNrI4iUfXc=";
-                };
-                config =
-                  let
-                    app = pkgs.writeScript "entry" (builtins.readFile ./sih-entrypoint.sh);
-                  in
-                  {
-                    entrypoint = [ app ];
-                  };
-              };
-            })
-          ];
+          overlays = pkgs-overlays;
         };
 
       in
@@ -132,9 +163,54 @@
           ];
         };
 
-        legacyPackages = pkgs;
         formatter = pkgs.nixfmt-tree;
+        legacyPackages = pkgs;
       }
-    );
+    )
+    // {
+      nixosConfigurations = {
+        flybrain-rpi5 = nixos-raspberrypi.lib.nixosSystemFull {
+          specialArgs = inputs;
 
+          modules = [
+            ({ nixos-raspberrypi, ... }: {
+              imports = with nixos-raspberrypi.nixosModules; [
+                raspberry-pi-5.base
+                # raspberry-pi-5.page-size-16k
+              ];
+            })
+            {
+              networking.hostId = "8821e309";
+            } # NOTE: for zfs, must be unique
+            ({ lib, ... }: {
+              nixpkgs.overlays = pkgs-overlays;
+              boot.loader.raspberry-pi.bootloader = "kernel";
+              system.stateVersion = "26.05";
+            })
+
+            # Without this there is no way to log into the box at all,
+            # over SSH or otherwise.
+            ({ ... }: {
+              services.openssh.enable = true;
+              users.users.root.openssh.authorizedKeys.keys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBIWROhXthac0SiUb/ZY/f4cRhvkrwaV8W4eXGaDe63K ben@nebs-desktop.lan"
+              ];
+            })
+
+            # Lets `nix build .#nixosConfigurations.flybrain-rpi5.config.system.build.diskoImages`
+            # build a flashable aarch64-linux .raw image on this x86_64-linux host, by running
+            # disko's installer VM under qemu/binfmt emulation instead of natively.
+            ({ pkgs, ... }: {
+              disko.imageBuilder.enableBinfmt = true;
+              environment.systemPackages = [ pkgs.rosPackages.kilted.ros-core ];
+            })
+
+            disko.nixosModules.disko
+            ./nix/nixos/rpi5-configtxt.nix
+            ./nix/nixos/disko-disk-config.nix
+          ];
+        };
+      };
+
+    };
 }
---
+++ nix/nixos/disko-disk-config.nix
@@ -0,0 +1,163 @@
+{ config, lib, ... }:
+
+let
+  firmwarePartition = lib.recursiveUpdate {
+    # label = "FIRMWARE";
+    priority = 1;
+
+    type = "0700"; # Microsoft basic data
+    attributes = [
+      0 # Required Partition
+    ];
+
+    size = "1024M";
+    content = {
+      type = "filesystem";
+      format = "vfat";
+      # mountpoint = "/boot/firmware";
+      mountOptions = [
+        "noatime"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=1min"
+      ];
+    };
+  };
+
+  espPartition = lib.recursiveUpdate {
+    # label = "ESP";
+
+    type = "EF00"; # EFI System Partition (ESP)
+    attributes = [
+      2 # Legacy BIOS Bootable, for U-Boot to find extlinux config
+    ];
+
+    size = "1024M";
+    content = {
+      type = "filesystem";
+      format = "vfat";
+      # mountpoint = "/boot";
+      mountOptions = [
+        "noatime"
+        "noauto"
+        "x-systemd.automount"
+        "x-systemd.idle-timeout=1min"
+        "umask=0077"
+      ];
+    };
+  };
+
+in
+{
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+  # networking.hostId is set somewhere else
+  services.zfs.autoScrub.enable = true;
+  services.zfs.trim.enable = true;
+
+  disko.devices = {
+    disk.nvme0 = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      # Only used when building a flashable .raw image via
+      # `disko.devices.disk.nvme0.imageName`/`system.build.diskoImages`
+      # (nix build .#nixosConfigurations.flybrain-rpi5.config.system.build.diskoImages).
+      # Not used for a live install (e.g. via nixos-anywhere), where disko
+      # partitions the real device and "100%" below means the whole disk.
+      # Keep this comfortably under your NVMe's real capacity (a "500GB"
+      # drive is usually ~465 GiB usable) and bump it if the build fails
+      # because the image is too small.
+      imageSize = "440G";
+      content = {
+        type = "gpt";
+        partitions = {
+
+          FIRMWARE = firmwarePartition {
+            label = "FIRMWARE";
+            content.mountpoint = "/boot/firmware";
+          };
+
+          ESP = espPartition {
+            label = "ESP";
+            content.mountpoint = "/boot";
+          };
+
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "rpool"; # zroot
+            };
+          };
+
+        };
+      };
+    }; # nvme0
+
+    zpool = {
+      rpool = {
+        type = "zpool";
+
+        # zpool properties
+        options = {
+          ashift = "12";
+          autotrim = "on"; # see also services.zfs.trim.enable
+        };
+
+        # zfs properties
+        rootFsOptions = {
+          # "com.sun:auto-snapshot" = "false";
+          # https://jrs-s.net/2018/08/17/zfs-tuning-cheat-sheet/
+          compression = "lz4";
+          atime = "off";
+          xattr = "sa";
+          acltype = "posixacl";
+          # https://rubenerd.com/forgetting-to-set-utf-normalisation-on-a-zfs-pool/
+          normalization = "formD";
+          dnodesize = "auto";
+          mountpoint = "none";
+          canmount = "off";
+        };
+
+        postCreateHook =
+          let
+            poolName = "rpool";
+          in
+          "zfs list -t snapshot -H -o name | grep -E '^${poolName}@blank$' || zfs snapshot ${poolName}@blank";
+
+        datasets = {
+
+          # stuff which can be recomputed/easily redownloaded, e.g. nix store
+          local = {
+            type = "zfs_fs";
+            options.mountpoint = "none";
+          };
+          "local/nix" = {
+            type = "zfs_fs";
+            options = {
+              reservation = "128M";
+              mountpoint = "legacy"; # to manage "with traditional tools"
+            };
+            mountpoint = "/nix"; # nixos configuration mountpoint
+          };
+
+          # _system_ data
+          system = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "none";
+            };
+          };
+          "system/root" = {
+            type = "zfs_fs";
+            options = {
+              mountpoint = "legacy";
+            };
+            mountpoint = "/";
+          };
+
+        };
+      };
+    };
+  };
+}
